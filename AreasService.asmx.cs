1f87dd70c0667b1ee16e61c66b45ae343e7f1b34
Jayivalo99 <91566499+Jayivalo99@users.noreply.github.com>
2021-09-28 14:27:17 -0500

    Add files via upload

---
+++ AreasService.asmx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace AreasWS.Calcular.SOAP
+{
+    /// <summary>
+    /// Descripción breve de AreasService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class AreasService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hola a todos";
+        }
+
+        [WebMethod]
+        public decimal cuadrado(decimal n1)
+        {
+            CalcularAreas c = new CalcularAreas();
+            c.numero1 = n1;
+           
+            return c.cuadrado(n1);
+        }
+        [WebMethod]
+        public decimal triangulo(decimal n1, decimal n2)
+        {
+            CalcularAreas c = new CalcularAreas();
+            c.numero1 = n1;
+            c.numero2 = n2;
+            return c.triangulo(n1, n2);
+        }
+        [WebMethod]
+        public double circulo(double n1)
+        {
+            CalcularAreas c = new CalcularAreas();
+            //c.numero1 = n1;
+            return c.circulo(n1);
+        }
+    }
+}
---
+++ CalcularAreas.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AreasWS.Calcular.SOAP
+{
+    public class CalcularAreas
+    {
+		public decimal numero1 { get; set; }
+		public decimal numero2 { get; set; }
+		
+		public decimal a;
+		
+
+
+		public decimal cuadrado(decimal n1)
+		{
+			numero1 = n1;
+
+			return n1 * n1;
+		}
+
+		public decimal triangulo(decimal n1, decimal n2)
+        {
+			numero1 = n1;
+			numero2 = n2;
+			a = n1 * n2;
+			return a / 2;
+		}
+
+		public double circulo(double n1)
+		{
+			return (n1 * n1) * 3.1416;
+		}
+	}
+}
